dd997732ffc8c9a7a79565c28a471d2040273fdf
clu23 <clubuntu@FRTOU-61P2C14.eu.hexagonmetrology.com>
2024-11-10 12:41:51 +0100

    adding gameboard class and modifying some of its properties

--- src/modules/factories/gameboard.js
+++ src/modules/factories/gameboard.js
@@ -5,6 +5,20 @@ import Ship from './ship.js'
 
 class GameBoard{
     constructor(){
-        
+        this.board=new Array(10).fill('x').map(() => new Array(10).fill('x'));
+        this.placeMode="X";
+    }
+
+    rotate(){
+        if (this.placeMode==="X"){
+            this.placeMode="Y";
+        }
+        else{
+            this.placeMode="X";
+        }
+    }
+
+    placeShip(x,y){
+
     }
 }
