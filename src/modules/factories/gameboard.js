c5e1ddcc135f50e6730d0875c02ddcdd29bd0cac
clu23 <clubuntu@FRTOU-61P2C14.eu.hexagonmetrology.com>
2024-12-09 12:28:25 +0100

    introducing isPlacementPossible function in gameboard module, still need to complete it

--- src/modules/factories/gameboard.js
+++ src/modules/factories/gameboard.js
@@ -1,11 +1,11 @@
 
 import Ship from './ship.js'
 
-
+const SIZE = 10
 
 class GameBoard{
     constructor(){
-        this.board=new Array(10).fill('x').map(() => new Array(10).fill('x'));
+        this.board=new Array(SIZE).fill('x').map(() => new Array(SIZE).fill('x'));
         this.placeMode="X";
         this.fleet=[];
     }
@@ -19,7 +19,26 @@ class GameBoard{
         }
     }
 
-    placeShip(x,y){
+    placeShip(ship,row,column){
+
+    }
+
+    isPlacementPossible(ship,row,column,placementMode){
+
+         // case position is out of gameboard
+        if (row<0 || row>SIZE-1 || column<0 || column>SIZE-1){
+            return false
+        }
+
+        // case ship doesn't fit in gameboard
+        if (placementMode==="X"){
+            if (column + ship.size > SIZE) return false
+        }
+        else{
+            if (row + ship.size > SIZE) return false
+        }
+
+        // case any of the fields is already taken
 
     }
 }
