aee07c477c15f51adf14b29ee05c25c5527f88d7
clu23 <clubuntu@FRTOU-61P2C14.eu.hexagonmetrology.com>
2025-01-12 17:59:48 +0100

    adding methods to Player class

--- src/modules/factories/player.js
+++ src/modules/factories/player.js
@@ -1,10 +1,46 @@
-import GameBoard from './gameboard'
+const GameBoard = require('./gameboard')
 
 
 class Player{
 
-    constructor(name,hitCoords){
+    constructor(name){
         this.name=name;
         this.hitCoords=[]
     }
+
+    attack(x,y,gameboard){
+        if (this.alreadyHit(x,y)){
+            return
+        }
+        this.hitCoords.push([x,y]);
+        gameboard.takeHit(x,y);
+
+    }
+
+    randomAttack(gameboard) {
+        if (this.hitCoords.length === 100) return
+    
+        let x = Math.floor(Math.random() * 10)
+        let y = Math.floor(Math.random() * 10)
+    
+        while (this.alreadyHit(x, y)) {
+          x = Math.floor(Math.random() * 10)
+          y = Math.floor(Math.random() * 10)
+        }
+    
+        this.hitCoords.push([x, y])
+        gameboard.takeHit(x, y)
+    }
+
+    alreadyHit(x,y) {
+        for (let i = 0; i < this.hitCoords.length; i++) {
+          if (this.hitCoords[i][0] === x && this.hitCoords[i][1] === y){
+            return true
+          }
+        }
+        return false
+      }
 }
+
+
+module.exports=Player
