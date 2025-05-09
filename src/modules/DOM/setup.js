15c69f401c510c1463dda0e84b0acf2d401a761b
clu23 <clubuntu@FRTOU-61P2C14.eu.hexagonmetrology.com>
2025-05-09 17:03:06 +0200

    working on the setup of the game through the file setup.js, added messages.js to handle the different messages provided to the player

--- src/modules/DOM/setup.js
+++ src/modules/DOM/setup.js
@@ -1,1 +1,27 @@
 import support from './support.js'
+import Game from '../factories/game.js'
+
+
+const setup = (() => {
+
+    function loadSetupContent() {
+        const app = document.getElementById('app')
+        app.classList.add('setup')
+    
+        app.appendChild(createSetupWrapper())
+    
+        displayWelcomeMessage()
+        initButtons()
+    }
+
+    function displayWelcomeMessage() {
+        const message = document.getElementById('message-agent')
+    
+        Component.addTypeWriterMessage(message, Message.getWelcomeMessage())
+    }
+
+
+})()
+
+
+export default setup
---
+++ src/modules/utils/messages.js
@@ -0,0 +1,18 @@
+import Game from '../factories/game.js'
+
+const Message = (() => {
+    const messages = {
+        welcome: [
+        'Welcome aboard, please select the axis and drag and drop ships on the map',
+        ],
+        battleStartPlayer: [
+        "We are ready Captain !",
+        ],
+        battleStartEnemy: [
+        "You should not have come here !",
+        ]
+    }
+
+})()
+
+export default Message
